b0d01744038f0d46cb59b8db60b959a04966f320
Pawan <pawan@Pawans-MacBook-Air.local>
2024-08-26 22:02:06 +0530

    Integrated profile data and fixed profile image issue

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import { Siderbar, MiddleOuterWraper, RightSidebar, CommonCard } from "../../components";
+import React, { useEffect, useState } from "react";
+import { Siderbar, MiddleOuterWraper, RightSidebar, CommonCard, Loading } from "../../components";
 import "./profile.scss";
 import { useNavigate } from "react-router-dom";
+import { _get } from "../../services/api";
 
 export default function Profile() {
   const profileData = {
@@ -26,6 +27,33 @@ export default function Profile() {
 
   const [activeTab, setActiveTab] = useState("posts");
   const navigate = useNavigate();
+  const [profileValues, setProfileValues] = useState([]);
+  const [isProfileLoading, setIsProfileLoading] = useState(false);
+
+  // This function is used for get profile data
+  const getProfileData = async () => {
+    setIsProfileLoading(true);
+    try {
+      const response = await _get(`social-media/profile`);
+      if (response?.status) {
+        setProfileValues(response?.data?.data);
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        console.error("Error Status Code:", error.response.status);
+        console.error("Error Message:", error.response.data.message);
+        showToast(error.response.data.message, "error");
+      } else {
+        console.error("An unknown error occurred.");
+      }
+    } finally {
+      setIsProfileLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProfileData();
+  }, []);
 
   const renderContent = () => {
     switch (activeTab) {
@@ -51,35 +79,42 @@ export default function Profile() {
         <Siderbar />
         <MiddleOuterWraper>
           <div className="container mt-4">
-            <div className="row">
-              <div className="col-md-3 text-center">
-                <img src={profileData.profilePic} alt="Profile" className="rounded-circle profile-pic" />
-              </div>
-              <div className="col-md-9">
-                <div className="d-flex align-items-center mb-3">
-                  <h2 className="me-3">{profileData.username}</h2>
-                  <button className="btn btn-outline-secondary btn-sm me-2" onClick={() => navigate("/edit-profile")}>
-                    Edit Profile
-                  </button>
-                  <button className="btn btn-outline-secondary btn-sm">Settings</button>
+            {isProfileLoading ? (
+              <Loading />
+            ) : (
+              <div className="row">
+                <div className="col-md-3 text-center">
+                  <img src={profileValues?.account?.avatar?.url} alt="Profile" className="rounded-circle profile-pic-box" />
                 </div>
-                <div className="d-flex mb-3">
-                  <div className="me-4">
-                    <strong>{profileData.posts}</strong> posts
+                <div className="col-md-9">
+                  <div className="d-flex align-items-center mb-3">
+                    <h2 className="me-3">{profileValues?.account?.username}</h2>
+                    <button className="btn btn-outline-secondary btn-sm me-2" onClick={() => navigate("/edit-profile")}>
+                      Edit Profile
+                    </button>
+                    <button className="btn btn-outline-secondary btn-sm">Settings</button>
                   </div>
-                  <div className="me-4">
-                    <strong>{profileData.followers}</strong> followers
+                  <div className="d-flex mb-3">
+                    <div className="me-4">
+                      <strong>{profileData.posts}</strong> posts
+                    </div>
+                    <div className="me-4">
+                      <strong>{profileValues?.followersCount}</strong> followers
+                    </div>
+                    <div>
+                      <strong>{profileValues.followingCount}</strong> following
+                    </div>
                   </div>
                   <div>
-                    <strong>{profileData.following}</strong> following
+                    <strong>
+                      {profileValues?.firstName} &nbsp;
+                      {profileValues?.lastName}
+                    </strong>
+                    <p>{profileValues.bio}</p>
                   </div>
                 </div>
-                <div>
-                  <strong>{profileData.fullName}</strong>
-                  <p>{profileData.bio}</p>
-                </div>
               </div>
-            </div>
+            )}
             <hr />
 
             <div className="container mt-4">
