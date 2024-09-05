2fdcde32fa83b3f197a82a977dd78ce18e6aa558
Pawan <pawan@Pawans-MacBook-Air.local>
2024-09-05 22:23:48 +0530

    Integrated deleted comment api

--- src/components/commentModal/CommentModal.jsx
+++ src/components/commentModal/CommentModal.jsx
@@ -4,7 +4,7 @@ import { useModal } from "../../services/hook/ModalContext";
 import "./commentModal.scss";
 import Loading from "../Loading";
 import { useToast } from "../../services/hook";
-import { _get, _post } from "../../services/api";
+import { _delete, _get, _post } from "../../services/api";
 
 export default function CommentModal({ commentId }) {
   const { modalShow, handleClose } = useModal();
@@ -12,6 +12,9 @@ export default function CommentModal({ commentId }) {
   const [isPostLoading, setIsPostLoading] = useState(false);
   const { showToast } = useToast();
   const [getCommentData, setGetCommentData] = useState([]);
+  const [hasCommentDeleteLoading, setHasCommentDeleteLoading] = useState(false);
+  const [deletedCommentId, setDeletedCommentId] = useState(null);
+  const [getCommentDataLoading, setGetCommentDataLoading] = useState(false);
 
   //   Post comment
   const postComment = async () => {
@@ -47,6 +50,7 @@ export default function CommentModal({ commentId }) {
 
   //   Get all comments
   const getAllCommentData = async () => {
+    setGetCommentDataLoading(true);
     try {
       const response = await _get(`social-media/comments/post/${commentId}?page=1&limit=100`);
       if (response?.status === 200) {
@@ -61,6 +65,7 @@ export default function CommentModal({ commentId }) {
         console.error("An unknown error occurred.");
       }
     } finally {
+      setGetCommentDataLoading(false);
     }
   };
   useEffect(() => {
@@ -69,19 +74,55 @@ export default function CommentModal({ commentId }) {
     }
   }, [commentId]);
 
+  // Delete comments
+  const deleteComments = async (deleteCommentId) => {
+    setDeletedCommentId(deleteCommentId);
+    setHasCommentDeleteLoading(true);
+    try {
+      const response = await _delete(`social-media/comments/${deleteCommentId}`);
+      if (response?.status === 200) {
+        getAllCommentData();
+        showToast(response.data.message, "success");
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        console.error("Error Status Code:", error.response.status);
+        console.error("Error Message:", error.response.data.message);
+        showToast(error.response.data.message, "error");
+      } else {
+        console.error("An unknown error occurred.");
+      }
+    } finally {
+      setHasCommentDeleteLoading(false);
+    }
+  };
+
   return (
     <>
       <CommonModal show={modalShow} handleClose={handleClose} title="Modal title">
         <div className="comment-scroll-section">
-          {getCommentData?.map((data, index) => (
-            <div className="comment-box-container" key={index}>
-              <img src={data?.author?.account?.avatar?.url ? data?.author?.account?.avatar?.url : "https://avatar.iran.liara.run/public"} alt="" className="comment-username-pic" />
-              <div>
-                <strong className="m-0">{data?.author?.account?.username}</strong>
-                <p>{data?.content}</p>
+          {getCommentDataLoading ? (
+            <Loading />
+          ) : (
+            getCommentData?.map((data, index) => (
+              <div className="comment-box-container" key={index}>
+                <img src={data?.author?.account?.avatar?.url ? data?.author?.account?.avatar?.url : "https://avatar.iran.liara.run/public"} alt="" className="comment-username-pic" />
+                <div>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <strong className="m-0">{data?.author?.account?.username}</strong>
+                    <div className="d-flex gap-2 mx-5">
+                      <button className="btn btn-primary">edit</button>
+                      <button className="btn btn-danger" onClick={() => deleteComments(data?._id)} disabled={hasCommentDeleteLoading && deletedCommentId === data?._id}>
+                        {hasCommentDeleteLoading && deletedCommentId === data?._id ? <Loading /> : "delete"}
+                      </button>
+                    </div>
+                  </div>
+
+                  <p>{data?.content}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
         <form onSubmit={handleCommentSubmit} className="mt-3">
           <div className="input-modal-box">
