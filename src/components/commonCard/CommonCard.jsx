be9ab64ea302e0db767f2b21b23b6490a3327eb8
Pawan <pawan@Pawans-MacBook-Air.local>
2024-09-12 21:59:54 +0530

    fixed card height issue on profile page

--- src/components/commonCard/CommonCard.jsx
+++ src/components/commonCard/CommonCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./commonCard.scss";
 
 export default function CommonCard({ data }) {
   return (
     <>
       <div className="col-4 p-1">
-        <img src={data?.images[0]?.url ? data?.images[0]?.url : "https://via.placeholder.com/300"} alt={`Photo`} className="img-fluid h-100" />
+        <img src={data?.images[0]?.url ? data?.images[0]?.url : "https://via.placeholder.com/300"} alt={`Photo`} className="img-fluid card-height" />
       </div>
     </>
   );
